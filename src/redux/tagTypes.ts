61b05a645db53dd1c9ebb98a3c716b1bc0c6d331
git-sujon <git.sujon@gmail.com>
2023-12-23 21:57:16 +0600

    Redux added, authApi, baseApi and axios helper added

---
+++ src/redux/tagTypes.ts
@@ -0,0 +1,13 @@
+export enum tagTypes {
+  auth = "auth",
+  users = "users",
+  products = "products",
+  carts = "carts",
+}
+
+export const tagTypesList = [
+  tagTypes.auth,
+  tagTypes.users,
+  tagTypes.products,
+  tagTypes.carts,
+];
