23d6cca7bad5373d8c5353d6f757b754f0c760ba
git-sujon <git.sujon@gmail.com>
2023-12-24 15:08:46 +0600

    getmycart and addToCart apiSlice created

---
+++ src/redux/api/cartApi.ts
@@ -0,0 +1,29 @@
+import { tagTypes } from "../tagTypes";
+import { baseApi } from "./baseApi";
+
+const urlExtension = "/carts";
+
+export const cartApi = baseApi.injectEndpoints({
+  endpoints: (build) => ({
+    getMyCart: build.query({
+      query: (arg: Record<string, any>) => ({
+        url: `${urlExtension}/get-my-cart`,
+        method: "GET",
+        params: arg,
+      }),
+
+      providesTags: [tagTypes.products],
+    }),
+
+    addToCart: build.mutation({
+        query: (data) => ({
+          url: `${urlExtension}/add-to-cart`,
+          method: "POST",
+          data: data,
+        }),
+        invalidatesTags: [tagTypes.auth],
+      }),
+  }),
+});
+
+export const {  } = cartApi;
